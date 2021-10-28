5a5aa7c46d80be27f8c0915a4d808c0c97b017c5
Elena <silena003@gmail.com>
2021-10-28 11:27:07 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_300;
+        int marginalCost = 20;
+        int totalMiles = ticketPrice / marginalCost;
+        System.out.println(totalMiles);
+    }
+}
